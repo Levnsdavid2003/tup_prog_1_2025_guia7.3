ac481ec20483d24c7f3b7ac91da2ee094cca171b
David L. <162506643+Levnsdavid2003@users.noreply.github.com>
2025-05-22 11:44:40 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PromediodeVariosNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont, acum, num, promedio;
+
+            Console.WriteLine("Escriba el primer número:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            cont = 0;
+            acum = 0;
+
+            while (num != -1)
+            {
+                acum = num + acum;
+                cont++;
+
+                Console.WriteLine("Escriba el siguiente número (o '-1' para corte):");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+
+            promedio = acum / cont;
+
+            Console.WriteLine("El promedio de los " + cont + " números es: " + promedio);
+            Console.ReadKey();
+        }
+    }
+}
