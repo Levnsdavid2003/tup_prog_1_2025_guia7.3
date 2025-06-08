fe79fca9628ba7e05c9979d326ca0d4eb55b6843
Levnsdavid2003 <levinezdavid@gmail.com>
2025-06-08 20:56:18 -0300

    agregado ejercicio4 y 5

---
+++ Guia7.3/ejercicio4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, mayor, menor, contador;
+            mayor = 0;
+            menor = 0;
+
+            for (contador = 1; contador <= 30; ++contador)
+            {
+                Console.WriteLine("Ingrese un número entero:");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num > mayor || contador == 1)
+                {
+                    mayor = num;
+                }
+                if (num < menor || contador == 1)
+                {
+                    menor = num;
+                }
+            }
+
+            Console.WriteLine($@"
+    El número mayor fué: {mayor}
+    El número menor fué: {menor}");
+        }
+    }
+}
---
+++ Guia7.3/ejercicio5/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int encuestados, positivos, negativos, indecisos;
+            double prcpositivo, prcnegativo, prcindeciso;
+
+
+            Console.WriteLine("--- Lista de encuestas ---\n");
+            Console.WriteLine(@"Ingrese la primer opinión:
+    0: Positiva
+    1: Negativa
+    2: Indeciso
+    Otro: Salir");
+            int opinion = Convert.ToInt32(Console.ReadLine());
+            encuestados = 0;
+            positivos = 0;
+            negativos = 0;
+            indecisos = 0;
+            prcpositivo = 0;
+            prcnegativo = 0;
+            prcindeciso = 0;
+
+            while (opinion >= 0 && opinion <= 2)
+            {
+                switch (opinion)
+                {
+                    case 0:
+                        positivos++;
+                        break;
+                    case 1:
+                        negativos++;
+                        break;
+                    case 2:
+                        indecisos++;
+                        break;
+                    default:
+                        Console.WriteLine("----- Fin de lista -----\n");
+                        break;
+                }
+
+                if (opinion >= 0 && opinion <= 2)
+                {
+                    encuestados++;
+                    Console.WriteLine(@"
+Ingrese la siguiente opinión:
+    0: Positiva
+    1: Negativa
+    2: Indeciso
+    Otro: Salir");
+                    opinion = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            if (encuestados > 0)
+            {
+                prcpositivo = (positivos * 100) / encuestados;
+                prcnegativo = (negativos * 100) / encuestados;
+                prcindeciso = (indecisos * 100) / encuestados;
+                Console.WriteLine($@"
+Resultados:
+    - Opiniones: {encuestados}
+    - Porcentaje de opiniones positivas: {prcpositivo:f2}%
+    - Porcentaje de opiniones negativas: {prcnegativo:f2}%
+    - Porcentaje de opiniones indecisas: {prcindeciso:f2}%");
+            }
+            else
+            {
+                Console.WriteLine("No hubo encuestados.");
+            }
+
+
+
+        }
+    }
+}
